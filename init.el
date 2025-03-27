667d186e582bb1867b69773da3ff8cb0faadc616
Daniel Nouri <daniel.nouri@gmail.com>
2025-03-27 15:41:56 +0100

    Update flymake/ruff config: replace with eglot

--- init.el
+++ init.el
@@ -90,41 +90,21 @@
       "breakpoint()")
 (global-set-key "\M-t" 'pdb-set)
 
-;; http://www.flycheck.org/en/latest/user/installation.html
-;; Ruff
-(require 'flycheck)
-;; From https://github.com/flycheck/flycheck/issues/1974#issuecomment-1343495202
-(setq flycheck-python-ruff-executable "/home/daniel/lib/venv/bin/ruff")
-(flycheck-define-checker python-ruff
-  "A Python syntax and style checker using the ruff utility.
-To override the path to the ruff executable, set
-`flycheck-python-ruff-executable'.
-See URL `http://pypi.python.org/pypi/ruff'."
-  :command ("ruff"
-            "--format=text"
-            (eval (when buffer-file-name
-                    (concat "--stdin-filename=" buffer-file-name)))
-            "-")
-  :standard-input t
-  :error-filter (lambda (errors)
-                  (let ((errors (flycheck-sanitize-errors errors)))
-                    (seq-map #'flycheck-flake8-fix-error-level errors)))
-  :error-patterns
-  ((warning line-start
-            (file-name) ":" line ":" (optional column ":") " "
-            (id (one-or-more (any alpha)) (one-or-more digit)) " "
-            (message (one-or-more not-newline))
-            line-end))
-  :modes python-mode)
+(set-default-coding-systems 'utf-8)
 
-;; Use something adapted to your config to add `python-ruff' to `flycheck-checkers'
-;; This is an MVP example:
-(setq python-mode-hook
-      (list (defun my-python-hook ()
-              (unless (bound-and-true-p org-src-mode)
-                (when (buffer-file-name)
-                  (setq-local flycheck-checkers '(python-ruff))
-                  (flycheck-mode))))))
+;; eglot
+(add-hook 'python-mode-hook 'eglot-ensure)
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs
+               '(python-mode . ("ruff" "server")))
+  ;; (add-hook 'after-save-hook 'eglot-format)
+)
+
+;; flymake-ruff
+;; (require 'flymake-ruff)
+;; (add-hook 'python-base-mode-hook 'flymake-mode)
+;; (add-hook 'python-mode-hook #'flymake-ruff-load)
+
 
 ;; Tramp
 (require 'tramp)
