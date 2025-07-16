50ca53ec23d9d6755f4fb2786524c15a1d0a5637
Daniel Nouri <daniel@redhotcar>
2025-07-16 14:17:46 +0200

    Better font selection

--- desktop.el
+++ desktop.el
@@ -4,59 +4,95 @@
 (load-theme 'doom-pine t)
 
 ;; Fonts
-(set-frame-font "Monospace-10")
-(set-frame-font "Monospace-14")
-(add-to-list 'default-frame-alist '(font . "Monospace-14"))
-(defun medium-fonts1 ()
-  "Medium fonts"
-  (interactive)
-  (set-frame-font "Cascadia Mono-14"))
-(defun large-fonts1 ()
-  "Large fonts"
-  (interactive)
-  (set-frame-font "Cascadia Mono-16"))
-(defun super-large-fonts1 ()
-  "Super large fonts"
-  (interactive)
-  (set-frame-font "Cascadia Mono-22"))
+;; This configuration provides a flexible way to manage fonts.
+;;
+;;  - F6: Decrease font size
+;;  - F7: Increase font size
+;;  - F8: Switch to the primary font (e.g., for coding)
+;;  - F9: Switch to the secondary font (e.g., for writing prose)
+;;
+;; The font size is preserved when switching between families.
 
-(global-set-key [M-f7] 'medium-fonts1)
-(global-set-key [M-f8] 'large-fonts1)
-(global-set-key [M-f9] 'super-large-fonts1)
+;; --- 1. Font Availability Check ---
 
-(defun medium-fonts2 ()
-  "Medium fonts"
-  (interactive)
-  (set-frame-font "Comic Code Daniel-14"))
-(defun large-fonts2 ()
-  "Large fonts"
-  (interactive)
-  (set-frame-font "Comic Code Daniel-16"))
-(defun super-large-fonts2 ()
-  "Super large fonts"
-  (interactive)
-  (set-frame-font "Comic Code Daniel-22"))
+(defun font-available-p (name)
+  "Check if font NAME is available to Emacs."
+  (not (null (x-list-fonts name))))
 
-(global-set-key [f7] 'medium-fonts2)
-(global-set-key [f8] 'large-fonts2)
-(global-set-key [f9] 'super-large-fonts2)
+;; --- 2. Define Your Fonts ---
 
-(defun medium-fonts2 ()
-  "Medium fonts"
-  (interactive)
-  (set-frame-font "Comic Code Daniel-14"))
-(defun large-fonts2 ()
-  "Large fonts"
-  (interactive)
-  (set-frame-font "Comic Code Daniel-16"))
-(defun super-large-fonts2 ()
-  "Super large fonts"
-  (interactive)
-  (set-frame-font "Comic Code Daniel-22"))
+(defvar my/font-primary-family
+  (cond ((font-available-p "Cascadia Mono") "Cascadia Mono")
+        ((font-available-p "Noto Sans Mono") "Noto Sans Mono")
+        (t "monospace"))
+  "The font family to use for programming and technical work.")
 
-(global-set-key [f7] 'medium-fonts2)
-(global-set-key [f8] 'large-fonts2)
-(global-set-key [f9] 'super-large-fonts2)
+(defvar my/font-secondary-family
+  (cond ((font-available-p "Comic Code") "Comic Code")
+        ((font-available-p "Comic Mono") "Comic Mono")
+        (t "monospace"))
+  "The font family to use for prose and creative writing.")
+
+(defvar my/font-default-size 14
+  "The default font size to start with (in points).")
+
+;; --- 3. Set the Default Font on Startup ---
+
+(set-face-attribute 'default nil
+                    :family my/font-primary-family
+                    :weight 'bold
+                    :height (* 10 my/font-default-size))
+
+;; --- 4. Core Font Control Functions ---
+
+(defun my/font-current-height ()
+  "Return the current font height in tenths of points."
+  (face-attribute 'default :height))
+
+(defun my/font-adjust-size (delta)
+  "Adjust the font size of the current frame by DELTA points.
+Preserves the current font family and weight."
+  (interactive)
+  (let* ((current-height (my/font-current-height))
+         (new-height (max 80 (+ current-height (* 10 delta))))) ;; prevent size < 8pt
+    (set-face-attribute 'default nil :height new-height)))
+
+(defun my/font-set-family (family)
+  "Set the font family for the current frame to FAMILY.
+Preserves the current font size and weight."
+  (interactive)
+  (let ((height (face-attribute 'default :height))
+        (weight (face-attribute 'default :weight)))
+    (set-face-attribute 'default nil :family family :weight weight :height height)))
+
+;; --- 5. Interactive Functions with Keybindings ---
+
+(defun my/font-decrease-size ()
+  "Decrease the current font size by 1 point."
+  (interactive)
+  (my/font-adjust-size -1))
+
+(defun my/font-increase-size ()
+  "Increase the current font size by 1 point."
+  (interactive)
+  (my/font-adjust-size +1))
+
+(defun my/font-switch-to-primary ()
+  "Switch to the primary font family."
+  (interactive)
+  (my/font-set-family my/font-primary-family))
+
+(defun my/font-switch-to-secondary ()
+  "Switch to the secondary font family."
+  (interactive)
+  (my/font-set-family my/font-secondary-family))
+
+;; --- 6. Assign Keys ---
+
+(global-set-key [f6] #'my/font-decrease-size)
+(global-set-key [f7] #'my/font-increase-size)
+(global-set-key [f8] #'my/font-switch-to-primary)
+(global-set-key [f9] #'my/font-switch-to-secondary)
 
 ;; shell mode
 (setq ansi-color-names-vector ; better contrast colors
